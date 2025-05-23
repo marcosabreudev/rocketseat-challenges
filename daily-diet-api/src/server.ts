ad787c9eaf14187e748a58cd6013772ea5242200
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2025-05-22 21:21:47 -0300

    feat: server

---
+++ daily-diet-api/src/server.ts
@@ -0,0 +1,32 @@
+import fastifyJwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { env } from './env'
+import { userRoutes } from './routes/users'
+import { mealRoutes } from './routes/meals'
+import { sessionRoute } from './routes/session'
+import { meRoute } from './routes/me'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(userRoutes, {
+  prefix: 'users',
+})
+app.register(mealRoutes, {
+  prefix: 'meals',
+})
+app.register(sessionRoute, {
+  prefix: 'sessions',
+})
+app.register(meRoute, {
+  prefix: 'me',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => console.log('App running'))
