02bc8e7994861b44b8f5373a75653af0878465aa
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2025-05-28 15:30:35 -0300

    fix: imports

--- nodejs-clean-ddd/src/domain/forum/application/use-cases/answer-question.spec.ts
+++ nodejs-clean-ddd/src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from "./answer-question";
 import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { Answer } from "../../enterprise/entities/answer";
 
 class AnswersLocalRepository implements AnswersRepository {
   async create(answer: Answer) {
