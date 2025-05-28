dbc9076531e6b3466385a68949dc03edd9f6b4fe
marcos abreu <32722681+marcosabreudev@users.noreply.github.com>
2025-05-28 15:18:17 -0300

    Module 01 (#1)

---
+++ nodejs-clean-ddd/src/core/types/optional.ts
@@ -0,0 +1,16 @@
+/**
+ * Make some property optional and type
+ *
+ * @example
+ * ```typescript
+ * type Post {
+ *  id: string;
+ *  name: string;
+ *  email: string;
+ * }
+ *
+ * Optional<Post, 'id' | 'email'>
+ * ```
+ */
+
+export type Option<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
